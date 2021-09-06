a093d5f7f90342439f84e8c1371a84970c126a1a
DmytroFedin <dimitriyfedin@gmail.com>
2021-09-06 16:49:47 +0300

    11

--- 11th-task/Slider.js
+++ 11th-task/Slider.js
@@ -3,32 +3,32 @@
 function check_value(fieldvalue) {
     switch (fieldvalue) {
     case 1:
-        document.getElementById("slider").innerHTML = "<img src='img/Nike-SB-Stefan-Janoski-Max-blue.png'>";
+        document.getElementById("slider").src='img/Nike-SB-Stefan-Janoski-Max-blue.png';
         break;
     case 2:
-        document.getElementById("slider").innerHTML = "<img src='img/Nike-SB-Stefan-Janoski-Max-white.png'>";
+        document.getElementById("slider").src='img/Nike-SB-Stefan-Janoski-Max-white.png';
         break;
     case 3:
-        document.getElementById("slider").innerHTML = "<img src='img/Nike-SB-Stefan-Janoski-Max-red.png'>";
+        document.getElementById("slider").src='img/Nike-SB-Stefan-Janoski-Max-red.png';
         break;
         case 4:
-        document.getElementById("slider").innerHTML = "<img src='img/Nike-SB-Stefan-Janoski-Max-teal.png'>";
+        document.getElementById("slider").src='img/Nike-SB-Stefan-Janoski-Max-teal.png';
         break;
 }
 }
 function check_value1(fieldvalue) {
     switch (fieldvalue) {
-    case 1:
-        document.getElementById("slider1").innerHTML = "<img src='img/Nike-SB-Stefan-Janoski-Max-blue.png'>";
-        break;
-    case 2:
-        document.getElementById("slider1").innerHTML = "<img src='img/Nike-SB-Stefan-Janoski-Max-white.png'>";
-        break;
-    case 3:
-        document.getElementById("slider1").innerHTML = "<img src='img/Nike-SB-Stefan-Janoski-Max-red.png'>";
-        break;
-        case 4:
-        document.getElementById("slider1").innerHTML = "<img src='img/Nike-SB-Stefan-Janoski-Max-teal.png'>";
-        break;
-}
+        case 1:
+            document.getElementById("slider1").src='img/Nike-SB-Stefan-Janoski-Max-blue.png';
+            break;
+        case 2:
+            document.getElementById("slider1").src='img/Nike-SB-Stefan-Janoski-Max-white.png';
+            break;
+        case 3:
+            document.getElementById("slider1").src='img/Nike-SB-Stefan-Janoski-Max-red.png';
+            break;
+            case 4:
+            document.getElementById("slider1").src='img/Nike-SB-Stefan-Janoski-Max-teal.png';
+            break;
+    }
 }
