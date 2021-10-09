7c7424f90d0cd8cdc186eb9c3eee54a2ea739715
DmytroFedin <dimitriyfedin@gmail.com>
2021-10-09 15:02:50 +0300

    added js

---
+++ Task-1 JS/.history/index_20211006155526.js
@@ -0,0 +1,98 @@
+// let company = prompt('fgfgf', '');
+// company=+company
+
+// if (company > 0) {
+//   alert ('1');
+// }
+//   else if (company < 0) { alert ('-1');
+// }
+//   else if (company == 0) {
+//     alert ('0')
+//   }
+// let message
+// login=''
+
+// message=(login == 'Сотрудник')?  'Привет':
+//  (login == 'Директор') ?  'Здравствуйте':
+//  (login == '') ?  'Нет логина':
+//    '';
+
+//   console.log(message)
+// alert( alert(1) && alert(2) );
+// let age = prompt("your age?",'')
+
+// if (age > 90 || age < 14){
+//   alert('Ты пидор')
+// }
+// else{
+//   alert('tikay z sela')
+// }
+
+
+// let login = prompt('insert your login', ''), pass;
+
+
+// if (login =='Admin'){
+//   pass=prompt('Password', '');
+
+//   if (pass =='Im head'){
+//     alert('Hello')
+//   }
+//   else if (pass==''){
+//     alert('hi')
+//   }
+//   else if(pass==null){
+//     alert('denied')
+//   }
+//   else{
+//     alert('hi')
+//   }
+// }
+// else if (login == null || login == ''){
+//   alert('denied');
+// }
+// else {
+//   alert('I dont know u')
+// }
+// outer: for (let i = 0; i < 3; i++) {
+
+//   for (let j = 0; j < 3; j++) {
+
+//     let input = prompt(`Значение на координатах (${i},${j})`, '');
+
+//     // если пустая строка или Отмена, то выйти из обоих циклов
+//     if (!input) break outer; // (*)
+
+//     // сделать что-нибудь со значениями...
+//   }
+// }
+
+// alert('Готово!');
+// for (let i = 2; i < 10; i++){
+//   if (i % 2 == 0){
+// alert( i );
+// } }
+
+// for (let i = 2; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     alert( i );
+//   }
+// }
+// let i = 0;
+// while(i < 3) {
+//   alert( `number ${i}!` );
+//   i++
+// }
+// let i
+// do{
+// i = prompt('dfdfdf?',0)}
+// while ( i < 100 && i);
+let n = prompt("fgff?",'')
+nextPrime:
+for ( let i = 2; i <= n; i++){
+  for (let j = 2; j < i; j++) { // проверить, делится ли число..
+    if (i % j == 0) continue nextPrime; // не подходит, берём следующее
+  }
+   alert(i);
+
+}
